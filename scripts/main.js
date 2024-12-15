41e54cb6978490e6f183f34ec0614557ddfc5028
Wolodya12 <klari1299@gmail.com>
2024-12-15 13:54:58 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -45,3 +45,17 @@ async function loadNews() {
 
 // Викликаємо функцію для завантаження новин
 loadNews();
+async function updateCryptoPrices() {
+  try {
+    const response = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum&vs_currencies=usd');
+    const data = await response.json();
+
+    document.getElementById('btc-price').innerText = $${data.bitcoin.usd};
+    document.getElementById('eth-price').innerText = $${data.ethereum.usd};
+  } catch (error) {
+    console.error('Помилка отримання даних', error);
+  }
+}
+
+updateCryptoPrices();
+setInterval(updateCryptoPrices, 60000); // Оновлювати кожну хвилину
